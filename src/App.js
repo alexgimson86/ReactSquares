95dc9a5d24f28a5cf587e67bfb6a4723a89c0d03
alexgimson86 <alexgimson@gmail.com>
2019-01-16 17:57:04 -0600

    rendered squares that change color

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Rectangle from 'react-rectangle';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: ['#B22222','#FFFF00','#32CD32','#00CED1','#0000CD','#FF00FF','#C71585', '#8B4513']
+    };
+  }
+  componentDidMount(){
+    this.interval = setInterval(()=>{
+      var tempArr =this.state.colors.slice(0);
+       var tempHex =  tempArr.shift();
+       tempArr.push(tempHex)
+       this.setState({
+         colors: tempArr
+       })
+    },1000);
+  }
+ 
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+    <Rectangle className="Rectangle">
+      <div style={{ background: this.state.colors[0] , width: '80%', height: '80%' , border: '5px solid ' + this.state.colors[1]}} />
+    </Rectangle>
+    <Rectangle className="Rectangle">
+      <div style={{ background: this.state.colors[2], width: '80%', height: '80%' , border: '5px solid ' + this.state.colors[3]}} />
+    </Rectangle>
+    <Rectangle className="Rectangle">
+      <div style={{ background: this.state.colors[4], width: '80%', height: '80%' , border: '5px solid ' + this.state.colors[5]}} />
+    </Rectangle>
+    <Rectangle className="Rectangle">
+      <div style={{ background: this.state.colors[6], width: '80%', height: '80%' , border: '5px solid ' +this.state.colors[7]}} />
+    </Rectangle>
+    </div>
     );
   }
 }
